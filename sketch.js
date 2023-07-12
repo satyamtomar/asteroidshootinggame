e97d941b27b009188f7a10c25365794b51f3a1c4
Satyam Tomar <saitamaitachi123@gmail.com>
2023-07-12 15:08:55 +0530

    satyam

--- sketch.js
+++ sketch.js
@@ -3,27 +3,33 @@ let enemies = [];
 let score = 0;
 function setup() {
   createCanvas(400, 400);
-  // setInterval(bulletsUsed,500)
+  
+  setInterval(bulletsCreated,250)
   for (let i = 1; i <= 10; i++) {
     let enemy = {
       x: random(10, width - 20),
       y: random(-800, 0),
     };
     enemies.push(enemy);
+    
   }
+  
 }
 function draw() {
-  background(100);
+  background('rgb(241,146,241)');
   rectMode(CENTER);
-  circle(mouseX, height - 50, 25);
+  fill('#9EA0C1')
+  triangle(mouseX-20,height-30,mouseX, height - 50, mouseX+20,height-30);
   text(`score: ${score}`, 20, 20);
   for (let bullet of bullets) {
     bullet.y -= 3;
+    fill('rgb(242,247,214)')
     circle(bullet.x, bullet.y, 10);
   }
 
   for (let enemy of enemies) {
     enemy.y += 1;
+    fill('rgb(158,158,212)')
     rect(enemy.x, enemy.y, 10);
     if (enemy.y > height) {
       text("YOU LOSE", width / 2, height / 2);
@@ -51,11 +57,20 @@ function newEnemy() {
   };
   enemies.push(enemy);
 }
-function mousePressed() {
-  // console.log('hi')
+function bulletsCreated()
+{
+  
   let bullet = {
     x: mouseX,
     y: height - 50,
   };
   bullets.push(bullet);
 }
+// function mousePressed() {
+//   // console.log('hi')
+//   let bullet = {
+//     x: mouseX,
+//     y: height - 50,
+//   };
+//   bullets.push(bullet);
+// }
